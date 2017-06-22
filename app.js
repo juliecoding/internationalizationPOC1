d6f24833ac50d5f011bfc411d30fa19891cc3a63
Julie Kohler <julie.kohler@incontact.com>
2017-06-22 10:34:43 -0600

    Translation hash

--- app.js
+++ app.js
@@ -13,6 +13,19 @@ app.config(function ($translateProvider) {
     BUTTON_LANG_EN: 'englisch',
     BUTTON_LANG_DE: 'deutsch'
   });
+  $translateProvider.translations('ru', {
+    TITLE: 'Zdravstvuytye',
+    FOO: 'Eto -- paragraph',
+    BUTTON_LANG_EN: 'angliskiy',
+    BUTTON_LANG_DE: 'nimyetskiy'
+  })
+  $translateProvider.translations('fr', {
+    TITLE: 'Bonjour',
+    FOO: 'C\'est un paragraph',
+    BUTTON_LANG_EN: 'En anglais',
+    BUTTON_LANG_DE: 'En allemand'
+  })
+
   $translateProvider.preferredLanguage('en');
 });
 
@@ -20,4 +33,25 @@ app.controller('Ctrl', function ($scope, $translate) {
   $scope.changeLanguage = function (key) {
     $translate.use(key);
   };
+
+  $translate('TITLE').then(function(title) {
+    $scope.title = title;
+  }, function(translationId) {
+    $scope.title = translationId;
+  });
+  $translate('FOO').then(function(foo) {
+    $scope.foo = foo;
+  }, function(translationId) {
+    $scope.paragraph = translationId;
+  });
+  $translate('BUTTON_LANG_EN').then(function(button_lang) {
+    $scope.button_lang_en = button_lang
+  }, function(translationId) {
+    $scope.button_lang_en = translationId;
+  })
+  $translate('BUTTON_LANG_DE').then(function(button_lang) {
+    $scope.button_lang_de = button_lang
+  }, function(translationId) {
+    $scope.button_lang_de = translationId;
+  })
 });
